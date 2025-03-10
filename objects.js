8f1ab7cf5b4bbe1a149b1d04c93a12319ced9272
Kelvinm <kelvinmurithi18@gmail.com>
2025-03-10 11:08:52 +0300

    added objects and nested them/modify

---
+++ objects.js
@@ -0,0 +1,40 @@
+const studentNames = ['Paul', 'Luke','Janetrix']
+const name1 = studentNames[0]
+// console.log(name1);
+
+const student = {
+    admissionNum:"MS0001",
+    studentName:"John",
+    age:21,
+    idNumber: 3509283,
+    email:"john@gmail.com",
+    address:{
+        city:"New york",
+        country:"USA",
+    }
+}
+
+const carTypes = ['toyota','volvo','bmw','mercedes']
+
+const cars = {
+    toyota:{
+        yom:2014,
+        color:"blue",
+        model:"crown"
+    },
+    volvo:{
+        yom:2018,
+        color:"white",
+        model:"V60"
+    },
+    bmw:{
+        yom:2020,
+        color:"Black",
+        model:"X6"
+    },
+    mercedes:{
+        yom:2017,
+        color:"grey",
+        model:"E250"
+    }
+}
